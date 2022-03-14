894737ae7d23fd74c2e156e424391853a7c4900d
Mohammed Chekrouni <mchekrouni@SMB.SQLI.COM>
2022-03-14 15:05:58 +0100

    add impl cat functionnality

--- Animal.java
+++ Animal.java
@@ -2,7 +2,20 @@ class Animal{
     public static String getDefaultGreeting() {
         return "hello!";
     }
+    public static String getCatGreeting() {
+        return "tweet!";
+    }
     public static void main(String[] args) {
-        System.out.println(getDefaultGreeting());
+        String fullArgumants = args[0];
+        String output = "";
+
+        if (fullArgumants.equalsIgnoreCase("cat")) {
+            output = getCatGreeting();
+        }
+        else 
+            output = getDefaultGreeting();
+
+        System.out.println(output);
     }
 }
+
