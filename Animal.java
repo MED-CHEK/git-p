9e60da05df1c6cde0db6aff1c4d8bc8ef5538ad0
Mohammed Chekrouni <mchekrouni@SMB.SQLI.COM>
2022-03-14 14:49:39 +0100

    add impl bird functionnality

--- Animal.java
+++ Animal.java
@@ -2,7 +2,17 @@ class Animal{
     public static String getDefaultGreeting() {
         return "hello!";
     }
+    public static String getBirdGreeting() {
+        return "tweet!";
+    }
     public static void main(String[] args) {
-        System.out.println(getDefaultGreeting());
+        String fullArgumants = args[0];
+        String output = "";
+
+        if (fullArgumants.equalsIgnoreCase("bird")) {
+            output = getBirdGreeting();
+        }
+        System.out.println(output);
     }
 }
+
