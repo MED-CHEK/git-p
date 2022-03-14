f35db6cbaaa743202fa0f2d591a64ad3ef042f35
Mohammed Chekrouni <mchekrouni@SMB.SQLI.COM>
2022-03-14 14:37:11 +0100

    default greeting

---
+++ Animal.java
@@ -0,0 +1,8 @@
+class Animal{
+    public static String getDefaultGreeting() {
+        return "hello!";
+    }
+    public static void main(String[] args) {
+        System.out.println(getDefaultGreeting());
+    }
+}
